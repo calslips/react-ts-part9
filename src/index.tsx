49320369884b2d06d8c574cf0c2564176ce08545
Caleb Phillips <csphillips5@gmail.com>
2021-12-07 15:17:25 -0500

    type react props

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const Welcome = ({ name }: { name: string }) => {
+  return <h1>Hello, {name}</h1>
+}
+
+const element = <Welcome name='Sara' />
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
